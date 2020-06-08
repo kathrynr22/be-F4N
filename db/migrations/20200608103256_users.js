4fb88e3945e059773cedd0ba5d9a3850f937f358
Kathryn Roberts <kathrynr22@Kathryns-MacBook-Air.local>
2020-06-08 10:40:43 +0100

    adds users table to migrations folder

---
+++ db/migrations/20200608103256_users.js
@@ -0,0 +1,16 @@
+exports.up = (knex) => {
+  return knex.schema.createTable("users", (usersTable) => {
+    usersTable.string("username", 20).primary();
+    usersTable.string("first_name", 20).notNullable();
+    usersTable.string("last_name", 20).notNullable();
+    usersTable.string("email", 25).notNullable();
+    usersTable.string("avatar_url");
+    usersTable.string("location", 4).notNullable();
+    usersTable.string("bio", 500);
+    usersTable.string("charity_name").references("charities.charity_name");
+  });
+};
+
+exports.down = (knex) => {
+  return knex.schema.dropTable("users");
+};
