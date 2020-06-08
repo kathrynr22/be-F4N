056208e1f9cf2cf14e8cc55a679eb1ae314162ff
Kathryn Roberts <kathrynr22@Kathryns-MacBook-Air.local>
2020-06-08 10:54:55 +0100

    adds comments table to migrations folder

---
+++ db/migrations/20200608105002_comments.js
@@ -0,0 +1,13 @@
+exports.up = (knex) => {
+  return knex.schema.createTable("comments", (commentsTable) => {
+    commentsTable.increments("comment_id").primary();
+    commentsTable.string("username").references("users.username");
+    commentsTable.integer("job_id").references("jobs.job_id");
+    commentsTable.timestamp("created_at").defaultTo(knex.fn.now());
+    commentsTable.string("body", 500).notNullable();
+  });
+};
+
+exports.down = (knex) => {
+  return knex.schema.dropTable("comments");
+};
