a044f05d30bea6e5ee0132eae703d2e78b0dc180
Kathryn Roberts <kathrynr22@Kathryns-MacBook-Air.local>
2020-06-09 09:43:24 +0100

    adds three index.js files

---
+++ db/data/development-data/index.js
@@ -0,0 +1,6 @@
+exports.charities = require("./charities");
+exports.comments = require("./comments");
+exports.jobs = require("./jobs");
+exports.skill = require("./skill");
+exports.user = require("./user");
+exports.userSkills = require("./user-skills");
---
+++ db/data/index.js
@@ -0,0 +1,12 @@
+const { NODE_ENV = "development" } = process.env;
+
+const devData = require("./development-data");
+const testData = require("./test-data");
+
+const data = {
+  production: devData,
+  development: devData,
+  test: testData,
+};
+
+module.exports = data[NODE_ENV];
