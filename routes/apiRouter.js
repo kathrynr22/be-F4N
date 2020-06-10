8140846e3c2ecadb11ac726746b11db211668149
Kathryn Roberts <kathrynr22@Kathryns-MacBook-Air.local>
2020-06-10 19:25:48 +0100

    adds endpoints

--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -2,9 +2,10 @@ const apiRouter = require("express").Router();
 const jobsRouter = require("./jobsRouter");
 const skillsRouter = require("./skillsRouter");
 const usersRouter = require("./usersRouter");
+const endpoints = require("../endpoints.json");
 
 apiRouter.route("/").get((req, res, next) => {
-  res.status(200);
+  res.status(200).send(endpoints);
 });
 
 apiRouter.use("/jobs", jobsRouter);
