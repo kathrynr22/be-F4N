cb49e3913144b49ce934c4afb29a9c4affe9c3ba
Kathryn Roberts <kathrynr22@Kathryns-MacBook-Air.local>
2020-06-11 15:38:18 +0100

    adds basic user authentication function

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const admin = require('firebase-admin');
+const serviceAccountKey = require('./serviceAccountKey.json');
 const cors = require('cors');
 const apiRouter = require('./routes/apiRouter');
 const {
@@ -6,13 +8,37 @@ const {
   handlePSQLErrors,
   handleCustomErrors,
 } = require('./controllers/errorControllers');
+const { NODE_ENV } = process.env;
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use('/api', apiRouter);
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccountKey),
+  databaseURL: 'https://f-4-n-a30d4.firebaseio.com',
+});
+
+function checkAuth(req, res, next) {
+  if (req.headers.authtoken) {
+    admin
+      .auth()
+      .verifyIdToken(req.headers.authtoken)
+      .then(() => {
+        next();
+      })
+      .catch(() => {
+        res.status(403).send('Unauthorized');
+      });
+  } else if (NODE_ENV === 'test') {
+    next();
+  } else {
+    res.status(403).send('Unauthorized');
+  }
+}
+
+app.use('/api', checkAuth, apiRouter);
 
 app.use(handlePSQLErrors);
 app.use(handleCustomErrors);
