d563bf2c36fa36fe7150ddb9e3d54b4924edd9cf
James Oliver <joliver.robert@me.com>
2020-06-12 12:46:37 +0100

    moves firebase checkAuth function into seperate firebase.js file

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
 const express = require('express');
-const admin = require('firebase-admin');
-const serviceAccountKey = require('./serviceAccountKey.json');
 const cors = require('cors');
 const apiRouter = require('./routes/apiRouter');
 const {
@@ -8,35 +6,12 @@ const {
   handlePSQLErrors,
   handleCustomErrors,
 } = require('./controllers/errorControllers');
-const { NODE_ENV } = process.env;
+const { checkAuth } = require('./firebase/firebase');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccountKey),
-  databaseURL: 'https://f-4-n-a30d4.firebaseio.com',
-});
-
-function checkAuth(req, res, next) {
-  if (req.headers.authtoken) {
-    admin
-      .auth()
-      .verifyIdToken(req.headers.authtoken)
-      .then(() => {
-        next();
-      })
-      .catch(() => {
-        res.status(403).send('Unauthorized');
-      });
-  } else if (NODE_ENV === 'test') {
-    next();
-  } else {
-    res.status(403).send('Unauthorized');
-  }
-}
 
 app.use('/api', checkAuth, apiRouter);
 
