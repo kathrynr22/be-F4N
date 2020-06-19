878b9c0dd65275f6bc8cf0f18011d2b891a50d33
Kathryn Roberts <kathrynr22@Kathryns-MacBook-Air.local>
2020-06-19 09:41:11 +0100

    adds notifications test

--- db/data/development-data/notifications.js
+++ db/data/development-data/notifications.js
@@ -1,19 +1,19 @@
 module.exports = [
   {
-    //comment_id added automatically by database
-    username: 'gdurdane',
-    body: 'You have received a comment on your job',
-    status: 'unread',
-  },
-  {
-    //comment_id added automatically by database
-    username: 'twebleyf',
-    body: 'You have received a comment on your job',
-    status: 'unread',
-  },
-  {
-    //comment_id added automatically by database
+    //hedge trimming help
     username: 'ebarkworth23',
+    body: 'You have received a comment on your job',
+    status: 'unread',
+  },
+  {
+    //tax return hell
+    username: 'cpevie21',
+    body: 'You have received a comment on your job',
+    status: 'unread',
+  },
+  {
+    //flatpack fans
+    username: 'kheasly20',
     body: 'You have received a comment on your job',
     status: 'unread',
   },
--- db/data/test-data/notifications.js
+++ db/data/test-data/notifications.js
@@ -1,14 +1,6 @@
-// exports.up = knex => {
-//   return knex.schema.createTable('notifications', notificationsTable => {
-//     notificationsTable.string('username').references('users.username');
-//     notificationsTable.string('body', 150).notNullable();
-//     notificationsTable.string('status').notNullable();
-//   });
-// };
-
 module.exports = [
   {
-    //comment_id added automatically by database
+    //job_id 1 hair stylist
     username: 'gdurdane',
     body: 'You have received a comment on your job',
     status: 'unread',
